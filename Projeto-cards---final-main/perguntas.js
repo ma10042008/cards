3bc45f6c08880ce7f91507b1b9ebc4f39fb52840
ma10042008 <161320836+ma10042008@users.noreply.github.com>
2024-11-11 07:59:45 -0300

    Add files via upload

---
+++ Projeto-cards---final-main/perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a área de um triângulo?',
+    'A área de um triângulo qualquer pode ser calculada, multiplicando a base pela sua altura e dividindo esse resultado por 2.'
+)
+criaCartao(
+    'Matemática',
+    'O que diz o teorema de pitágoras?',
+    'Em todo triângulo retângulo a soma dos quadrados dos catetos é igual ao quadrado da hipotenusa'
+)
+criaCartao(
+    'Matemática',
+    'como se calcula a area de um triangulo?',
+    'multiplicamos o quadrado da media do lado pela raiz de 3 dividido por 4'
+)
+criaCartao(
+    'Matemática',
+    'O que e javaScript?',
+    'e´uma poderosa linguagem de programação que pode adicionar interatividade a um site'
+)
+criaCartao(
+    'Matemática',
+    'O que e fotossintese?',
+    'e o processo pelo qual as plantas convertem luz solar e energia '
+)
+criaCartao(
+    'Matemática',
+    'Qual o maior planeta do sistema solar?',
+    'jupiter'
+)
+criaCartao(
+    'Matemática',
+    'Qual a capital do brasil?',
+    'brasilia'
+)
